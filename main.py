91daf38f5c6e3640f686efcc41ab6ca3cdcce5db
Christopher Chung <cskywalker1@gmail.com>
2019-06-22 15:52:26 +0100

    Made some more advancements.

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from time import sleep
 from names import rand_female_name, rand_gender, rand_male_name, rand_species
 
 
-# IDEA: Insert vet to increase health to max health. Can vet increase stat?
+# IDEA: Insert vet to increase health to max health.
 # IDEA:
 class Owner:
     def __init__(self):
         self.name = None
         self.money = 100
-        self.energy = None  # This determines how much an owner can do in a day.
+        self.energy = 3  # This determines how much an owner can do in a day.
 
 
 class Dog:
@@ -46,6 +46,25 @@ class Dog:
                                                                                                    self.hunger)
 
 
+# Prints out the dog's stats into a UI.
+def print_dog_stats_ui():
+    health_bar = '[{0:{1:}}]'.format('-' * OwnerDog.temp_health, OwnerDog.health)
+    social_bar = '[{0:{1:}}]'.format('-' * OwnerDog.temp_social, OwnerDog.social)
+    hunger_bar = '[{0:{1:}}]'.format('-' * OwnerDog.temp_hunger, OwnerDog.hunger)
+
+    dog_name_ui = "Name    : {}".format(OwnerDog.name)
+    gender_ui = "Gender  : {}".format(OwnerDog.gender)
+    species_ui = "Species : {}".format(OwnerDog.species)
+    health_ui = "Health  : {0:14} {1:}/{2:}".format(health_bar, OwnerDog.temp_health, OwnerDog.health)
+    social_ui = "Social  : {0:14} {1:}/{2:}".format(social_bar, OwnerDog.temp_social, OwnerDog.social)
+    hunger_ui = "Hunger  : {0:14} {1:}/{2:}".format(hunger_bar, OwnerDog.temp_hunger, OwnerDog.hunger)
+
+    print("{}\n{}\n{}\n{}\n{}\n{}".format(dog_name_ui, gender_ui, species_ui, health_ui, social_ui,
+                                          hunger_ui))
+    print("\nWhat Will You Do Today?")
+    print("[1] Walk {0:} \n[2] Feed {0:} \n[3] Play with {0:} \n[4] Go to Work".format(OwnerDog.name))
+
+
 if __name__ == "__main__":
     print("Christopher's Dog Owner Simulator 2019")
     player_owner = Owner()
@@ -63,21 +82,18 @@ if __name__ == "__main__":
             print("Invalid Selection\n")
             sleep(0.5)
 
-    Day = 0
-
-
-    def print_ui():
-        health_bar = '[{0:{1:}}]'.format('-' * OwnerDog.temp_health, OwnerDog.health)
-        social_bar = '[{0:{1:}}]'.format('-' * OwnerDog.temp_social, OwnerDog.social)
-        hunger_bar = '[{0:{1:}}]'.format('-' * OwnerDog.temp_hunger, OwnerDog.hunger)
-
-        dog_name_ui = "Name    : {}".format(OwnerDog.name)
-        species_ui = "Species : {}".format(OwnerDog.species)
-        health_ui = "Health  : {0:12} {1:}/{2:}".format(health_bar, OwnerDog.temp_health, OwnerDog.health)
-        social_ui = "Social  : {0:12} {1:}/{2:}".format(social_bar, OwnerDog.temp_social, OwnerDog.social)
-        hunger_ui = "Hunger  : {0:12} {1:}/{2:}".format(hunger_bar, OwnerDog.temp_hunger, OwnerDog.hunger)
-
-        print("{}\n{}\n{}\n{}\n{}".format(dog_name_ui, species_ui, health_ui, social_ui, hunger_ui))
-
-
-    print_ui()
+    Day = 1
+    while True:
+        print("Day {}".format(Day))
+        print_dog_stats_ui()
+        player_input = str(input())
+        if player_input == '1':
+            pass
+        elif player_input == '2':
+            pass
+        elif player_input == '3':
+            pass
+        elif player_input == '4':
+            pass
+        else:
+            print("Invalid Input")
