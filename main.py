d5811502a05a4bd802da90aa1342868fa7478b9c
Christopher Chung <cskywalker1@gmail.com>
2019-06-23 21:23:11 +0100

    Added endgame stats.

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ class Owner:
         self.money = 5
         self.energy = 3  # This determines how much an owner can do in a day.
 
+        # Stats for the end of the game.
+        self.times_vet = 0
+        self.vet_bill = 0
+        self.times_walk = 0
+        self.times_play = 0
+        self.times_fed = 0
+        self.food_bill = 0
+        self.income = 0
+
 
 class Dog:
     def __init__(self):
         self.gender = rand_gender()
-        self.status = True  # This will turn false when dog is dead.
+        self.alive = True  # This will turn false when dog is dead.
+        self.status = None
 
         # Creates name for dog based on gender.
         if self.gender == 'Male':
@@ -92,6 +102,23 @@ def print_prompt():
         OwnerDog.name))
 
 
+def print_endgame_report():
+    print("""
+    RSPCA Owner Report #{:05}
+    
+    Name:         {}
+    Dog Name:     {}
+    Dog Status:   {}
+    Vet Visits:   {}   (£{})
+    Times Walked: {}
+    Times Played: {}
+    Times Fed:    {}   (£{})
+    Owner Income: £{}""".format(randint(1, 99999), player_owner.name, OwnerDog.name, OwnerDog.status,
+                                player_owner.times_vet,
+                                player_owner.vet_bill, player_owner.times_walk, player_owner.times_play,
+                                player_owner.times_fed, player_owner.food_bill, player_owner.income))
+
+
 if __name__ == "__main__":
     print("Christopher's Dog Owner Simulator 2019")
     player_owner = Owner()
@@ -111,7 +138,7 @@ if __name__ == "__main__":
             sleep(0.5)
 
     Day = 0
-    while OwnerDog.status is True:
+    while OwnerDog.alive is True:
 
         OwnerDog.simulate_day()
         player_owner.energy = 3
@@ -124,20 +151,21 @@ if __name__ == "__main__":
             # Simulates chance of dog being taken for neglect by RSPCA.
             if randint(1, 5) == 5:
                 if player_owner.money >= 30:
-                    print("You bribed the RSPCA £30 to prevent them from taking {} for neglect!".format(OwnerDog.name))
+                    print("\nYou bribed the RSPCA £30 to prevent them from taking {} for neglect!".format(
+                        OwnerDog.name))
                     player_owner.money -= 30
                 else:
-                    print_dog_stats_ui()
-                    print("{} Was Taken by the RSPCA!".format(OwnerDog.name))
-                    OwnerDog.status = False
+                    print("\n{} Was Taken by the RSPCA!".format(OwnerDog.name))
+                    OwnerDog.alive = False
+                    OwnerDog.status = "Taken to RSPCA shelter"
 
         # If health of the dog is 0.
         if OwnerDog.temp_health == 0:
-            print_dog_stats_ui()
-            print('{} Died of Neglect!'.format(OwnerDog.name))
-            OwnerDog.status = False
+            print('\n{} Died of Neglect!'.format(OwnerDog.name))
+            OwnerDog.alive = False
+            OwnerDog.status = "Died from neglect"
 
-        while player_owner.energy > 0 and OwnerDog.status is True:
+        while player_owner.energy > 0 and OwnerDog.alive is True:
             print_prompt()
             player_input = str(input())
 
@@ -145,63 +173,73 @@ if __name__ == "__main__":
             if player_input == '1':
                 if player_owner.money < 5:
                     print("Insufficient Funds!")
-                    sleep(0.5)
+                    sleep(1)
                     print_dog_stats_ui()
-                    continue
-                print("You took {} to the vet and got billed £5!!".format(OwnerDog.name))
-                sleep(0.5)
-                OwnerDog.temp_health = OwnerDog.health
-                player_owner.money -= 5
-                player_owner.energy -= 1
-                print_dog_stats_ui()
+                else:
+                    print("You took {} to the vet and got billed £5!!".format(OwnerDog.name))
+                    sleep(0.5)
+
+                    OwnerDog.temp_health = OwnerDog.health
+                    player_owner.money -= 5
+                    player_owner.energy -= 1
+
+                    player_owner.times_vet += 1
+                    player_owner.vet_bill += 5
 
             # Walk
             elif player_input == '2':
-                print("You took {} for a walk!".format(OwnerDog.name))
-                sleep(0.5)
                 OwnerDog.temp_exercise += randint(1, 2)
                 if OwnerDog.temp_exercise > OwnerDog.exercise:
                     OwnerDog.temp_exercise = OwnerDog.exercise
+                print("You took {} for a walk!".format(OwnerDog.name))
+                sleep(0.5)
+
                 player_owner.energy -= 1
-                print_dog_stats_ui()
+
+                player_owner.times_walk += 1
 
             # Play
             elif player_input == '3':
-                print("You played with {}!".format(OwnerDog.name))
-                sleep(0.5)
                 OwnerDog.temp_social += randint(1, 2)
                 if OwnerDog.temp_social > OwnerDog.social:
                     OwnerDog.temp_social = OwnerDog.social
+
+                print("You played with {}!".format(OwnerDog.name))
+                sleep(0.5)
+
                 player_owner.energy -= 1
-                print_dog_stats_ui()
+
+                player_owner.times_play += 1
 
             # Feed
             elif player_input == '4':
                 if player_owner.money < 1:
                     print("Insufficient Funds")
-                    continue
-                OwnerDog.temp_hunger += randint(1, 2)
-                if OwnerDog.temp_hunger > OwnerDog.hunger:
-                    OwnerDog.temp_hunger = OwnerDog.hunger
-                player_owner.money -= 1
-                player_owner.energy -= 1
-                print('You spent £1 to feed {}!'.format(OwnerDog.name))
-                sleep(0.5)
-                print_dog_stats_ui()
+                    sleep(0.5)
+                else:
+                    OwnerDog.temp_hunger += randint(1, 2)
+                    if OwnerDog.temp_hunger > OwnerDog.hunger:
+                        OwnerDog.temp_hunger = OwnerDog.hunger
+                    player_owner.money -= 1
+                    player_owner.energy -= 1
+                    print('You spent £1 to feed {}!'.format(OwnerDog.name))
+                    sleep(0.5)
+
+                    player_owner.times_fed += 1
+                    player_owner.food_bill += 1
 
             # Work
             elif player_input == '5':
                 money_earned = randint(1, 3)
                 print("You Earned £{}!".format(money_earned))
                 player_owner.money += money_earned
+                player_owner.income += money_earned
                 player_owner.energy -= 1
                 sleep(0.5)
-                print_dog_stats_ui()
 
             else:
                 print("Invalid Input")
                 sleep(0.5)
-                print_dog_stats_ui()
-                continue
-        sleep(0.5)
+            print_dog_stats_ui()
     print("You Lose!")
+    print_endgame_report()
